9e01a6abe3862a53269eac769bbf19652623587c
Stanislas P1kachu Lejay <p1kachu@lse.epita.fr>
2017-04-30 21:21:16 +0200

    Add (supposed) handling for outb
    Signed-off-by: Stanislas P1kachu Lejay <p1kachu@lse.epita.fr>

--- hvf.c
+++ hvf.c
@@ -7,6 +7,7 @@
 #include "qemu/main-loop.h"
 #include "sysemu/accel.h"
 #include "sysemu/hvf.h"
+#include "exec/address-spaces.h"
 
 
 bool hvf_allowed = true;
@@ -42,25 +43,45 @@ static int hvf_init(MachineState *ms)
         return 0;
 }
 
-hv_return_t hvf_handle_io(CPUState *cpu, uint64_t exit_qual, uint64_t ins_len)
+static hv_return_t hvf_handle_io(CPUState *cpu, uint64_t exit_qual, uint64_t ins_len)
 {
         uint32_t access_size  = (exit_qual & 7) + 1;
-        uint32_t is_in        = (exit_qual & (1 << 3)) != 0;
+        uint32_t is_out       = (exit_qual & (1 << 3)) == 0;
         uint32_t is_string    = (exit_qual & (1 << 4)) != 0;
-        uint32_t is_rep       = (exit_qual & (1 << 5)) != 0;
-        uint32_t is_immediate = (exit_qual & (1 << 6)) != 0;
+        //uint32_t is_rep       = (exit_qual & (1 << 5)) != 0;
+        //uint32_t is_immediate = (exit_qual & (1 << 6)) != 0;
         uint32_t port         = exit_qual >> 16;
 
-        // Handling missing
-
-        uint64_t rip;
+        uint64_t rip, rax, count;
         hv_rd_reg(cpu->vcpuid, HV_X86_RIP, &rip);
+        hv_rd_reg(cpu->vcpuid, HV_X86_RAX, &rax);
+
+        uint8_t *data = (uint8_t *)rax;
+
+        X86CPU *x86_cpu = X86_CPU(cpu);
+        CPUX86State *env = &x86_cpu->env;
+        MemTxAttrs attrs = (MemTxAttrs) { .secure = (env->hflags & HF_SMM_MASK) != 0 };
+
+        count = 1; // TODO: Add proper handling
+
+        for (int i = 0; i < count; ++i) {
+                address_space_rw(&address_space_io,
+                                port,
+                                attrs,
+                                data,
+                                access_size,
+                                is_out);
+                data += access_size;
+        }
+
         hv_wr_reg(cpu->vcpuid, HV_X86_RIP, rip + ins_len);
+
+        return 0;
 }
 
 hv_return_t hvf_vcpu_exec(CPUState *cpu)
 {
-        uint64_t intr_info, tmp, exit_reason, qualification, ins_len;
+        uint64_t intr_info, tmp, exit_reason, qualification, ins_len, curr_rip;
         hv_return_t ret = 0;
         hv_vcpuid_t vcpu = cpu->vcpuid;
 
@@ -78,6 +99,9 @@ hv_return_t hvf_vcpu_exec(CPUState *cpu)
         exit_reason = hvf_get_exit_reason(vcpu) & 0xffff;
         hv_rd_vmcs(vcpu, VMCS_RO_VMEXIT_INSTR_LEN, &ins_len);
         hv_rd_vmcs(vcpu, VMCS_RO_EXIT_QUALIFIC, &qualification);
+        hv_rd_reg(vcpu, HV_X86_RIP, &curr_rip);
+
+        qemu_mutex_lock_iothread();
 
         hvf_debug_print_vmexit(exit_reason);
 
@@ -111,7 +135,6 @@ hv_return_t hvf_vcpu_exec(CPUState *cpu)
                         abort(); // TODO: Remove
         }
 
-        qemu_mutex_lock_iothread();
 
         return ret;
 }
