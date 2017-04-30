a09fd08c62ee248566527c34fd3241c8bc764114
Stanislas P1kachu Lejay <p1kachu@lse.epita.fr>
2017-04-30 22:29:18 +0200

    Comment supposed useless code out
    Signed-off-by: Stanislas P1kachu Lejay <p1kachu@lse.epita.fr>

--- hvf_memory.c
+++ hvf_memory.c
@@ -13,8 +13,8 @@ MemoryListener memory_listener;
 MemoryListener io_memory_listener;
 
 static void hvf_region_update(MemoryListener *listener,
-                              MemoryRegionSection *section,
-                              bool add)
+                MemoryRegionSection *section,
+                bool add)
 {
 
         MemoryRegion *mr = section->mr;
@@ -44,67 +44,80 @@ static void hvf_region_update(MemoryListener *listener,
                 }
         }
 
+        // TODO: Correct access rights (exploitation heaven right now)
         flags = HV_MEMORY_WRITE | HV_MEMORY_READ | HV_MEMORY_EXEC;
 
         void *ram = memory_region_get_ram_ptr(mr) +
-                    section->offset_within_region +
-                    delta;
+                section->offset_within_region +
+                delta;
 
-        // TODO: Correct access rights (exploitation heaven right now)
         hv_return_t ret;
         if (add) {
                 ret = hv_vm_map(ram, start_addr, size, flags);
                 DPRINTF("HVF: \033[32;1mhvf_region_add\033[0m(0x%016llx - "
-                        "0x%016llx) - ram: %llx - flags: %llx ",
-                        start_addr, start_addr + size, (uint64_t)ram, flags);
+                                "0x%016llx) - ram: %llx - flags: %llx ",
+                                start_addr, start_addr + size, (uint64_t)ram, flags);
                 DPRINTF(" = %x\n", ret);
         } else {
                 ret = hv_vm_unmap(start_addr, size);
                 DPRINTF("HVF: \033[31;1mhvf_region_del\033[0m(0x%016llx - "
-                        "0x%016llx)", start_addr, size + start_addr);
+                                "0x%016llx)", start_addr, size + start_addr);
                 DPRINTF(" = %x\n", ret);
         }
 }
 
 static void hvf_region_add(MemoryListener *listener,
-                           MemoryRegionSection *section)
+                MemoryRegionSection *section)
 {
         memory_region_ref(section->mr);
         hvf_region_update(listener, section, HVF_REGION_ADD);
 }
 
 static void hvf_region_del(MemoryListener *listener,
-                           MemoryRegionSection *section)
+                MemoryRegionSection *section)
 {
-
         hvf_region_update(listener, section, HVF_REGION_DELETE);
         memory_region_unref(section->mr);
 }
 
-static void hvf_io_region_add(MemoryListener *listener,
-                              MemoryRegionSection *section)
-{
 #if 0
+static void hvf_io_region_update(MemoryListener *listener,
+                MemoryRegionSection *section,
+                bool add)
+{
+
+        MemoryRegion *mr = section->mr;
         uint64_t size = int128_get64(section->size);
         uint64_t start_addr = section->offset_within_address_space;
-        DPRINTF("HVF: \033[32;1mhvf_io_region_add\033[0m(0x%016llx - "
-                "0x%016llx)\n",
-                start_addr, start_addr + size);
-#endif
+
+        hv_return_t ret;
+        if (add) {
+                //ret = hv_vm_map(ram, start_addr, size, flags);
+                DPRINTF("HVF: \033[32;1mhvf_io_region_add\033[0m(0x%016llx - "
+                                "0x%016llx)\n",
+                                start_addr, start_addr + size);
+        } else {
+                //ret = hv_vm_unmap(start_addr, size);
+                DPRINTF("HVF: \033[31;1mhvf_io_region_del\033[0m(0x%016llx - "
+                                "0x%016llx)\n",
+                                start_addr, size + start_addr);
+        }
+}
+
+static void hvf_io_region_add(MemoryListener *listener,
+                MemoryRegionSection *section)
+{
+        memory_region_ref(section->mr);
+        hvf_io_region_update(listener, section, HVF_REGION_ADD);
 }
 
 static void hvf_io_region_del(MemoryListener *listener,
-                              MemoryRegionSection *section)
+                MemoryRegionSection *section)
 {
-#if 0
-        uint64_t size = int128_get64(section->size);
-        uint64_t start_addr = section->offset_within_address_space;
-        DPRINTF("HVF: \033[31;1mhvf_io_region_del\033[0m(0x%016llx - "
-                "0x%016llx)\n",
-                start_addr, size + start_addr);
-#endif
+        hvf_io_region_update(listener, section, HVF_REGION_DELETE);
+        memory_region_unref(section->mr);
 }
-
+#endif
 
 hv_return_t hvf_memory_init(MachineState *ms)
 {
@@ -114,10 +127,11 @@ hv_return_t hvf_memory_init(MachineState *ms)
         memory_listener.region_del = hvf_region_del;
         memory_listener_register(&memory_listener, &address_space_memory);
 
+#if 0
         io_memory_listener.region_add = hvf_io_region_add;
         io_memory_listener.region_del = hvf_io_region_del;
         memory_listener_register(&io_memory_listener, &address_space_io);
-
+#endif
         return 0;
 }
 
